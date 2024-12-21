461a6a26bdda1d3e918558a35355a43b8f0c50b7
lijinhai255 <18854109500@163.com>
2024-12-21 08:55:27 +0800

    add:address for mongo

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
 };
 
 export default nextConfig;
--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -3,8 +3,8 @@ import { MongoClient, Db } from "mongodb";
 let client: MongoClient;
 let db: Db;
 
-const uri = process.env.DATABASE_URL || ""; // Load the DATABASE_URL from .env
-console.log("MONGO_URI:", process.env.MONGO_URI, uri, "uri-uri");
+const uri = process.env.NEXT_PUBLIC_DATABASE_URL || ""; // Load the DATABASE_URL from .env
+console.log("MONGO_URI:", process.env.NEXT_PUBLIC_DATABASE_URL, uri, "uri-uri");
 if (!uri) {
   throw new Error(
     "Please define the DATABASE_URL environment variable inside .env"
