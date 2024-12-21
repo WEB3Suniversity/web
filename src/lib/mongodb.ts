27ef226527188b79b4717e289f2f262e2d3884d2
lijinhai255 <18854109500@163.com>
2024-12-21 08:29:09 +0800

    add:address for mongo

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -3,7 +3,9 @@ import { MongoClient, Db } from "mongodb";
 let client: MongoClient;
 let db: Db;
 
-const uri = process.env.DATABASE_URL || ""; // Load the DATABASE_URL from .env
+const uri =
+  process.env.DATABASE_URL ||
+  "mongodb+srv://lijinhai255:ADeQx1Hyj8JRDZtF@cluster0.m2ayv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; // Load the DATABASE_URL from .env
 console.log("MONGO_URI:", process.env.MONGO_URI, uri, "uri-uri");
 if (!uri) {
   throw new Error(
